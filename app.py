9e96125abb37afcf70727e51cc0aa2f07eb97c46
Abhijeet Gupta <abhi.jeet_gupta@yahoo.com>
2020-06-16 21:23:06 +0530

    check_again

--- app.py
+++ app.py
@@ -20,18 +20,32 @@ def predict():
 
     output = round(prediction[0], 2)
 
-    return render_template('index.html', prediction_text='student_performance is {} where low:0, medium:1 and high:2'.format(output))
+
+    return render_template('index.html', 
+        prediction_text='student_performance is {} where low:0, medium:1 and high:2'.format(output),
+        Influencers_text='If student performace is poor then it is mainly due to Prom_Ponderado, Impres/Division5, Intra/Division1',
+        show_image=1
+        )
+
+
 
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
     '''
     For direct API calls trought request
     '''
+    print("reached here")
     data = request.get_json(force=True)
+    print("check")
     prediction = model.predict([np.array(list(data.values()))])
+    print("modle done")
 
     output = prediction[0]
-    return jsonify(output)
+    json_output={"output": output}
+    print(json_output)
+
+    print("modeldone1")
+    return jsonify(json_output)
 
 if __name__ == "__main__":
     app.run(debug=True)
